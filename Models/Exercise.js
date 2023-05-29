c257c3b68da70717a1439dcb9246df7e034d4d15
GiovanniCortes <giocortes19@gmail.com>
2023-05-29 13:13:38 -0400

    Created models for User and Exercise data

---
+++ Models/Exercise.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const ExerciseSchema = new mongoose.Schema({
+    description: {
+        type: String,
+    },
+    duration: {
+        type: Number
+    },
+    date: {
+        type: Date
+    },
+    _id: {
+        type: String,
+        required: [true, "Please insert the user ID"]
+    }
+})
+
+module.exports = mongoose.model('Exercise', ExerciseSchema)
