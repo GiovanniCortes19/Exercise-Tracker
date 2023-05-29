d4d05829cc5c29a28cded1171287ee995fbd3c0b
GiovanniCortes <giocortes19@gmail.com>
2023-05-29 13:51:33 -0400

    controllers to createUser and to getAllUsers created

---
+++ Controllers/register.js
@@ -0,0 +1,25 @@
+const User = require('../Models/User')
+
+const createUser = async (req, res) => { 
+    try {
+        const user = await User.create({...req.body})
+        res.status(200).json(user)
+    } catch (error) {
+        console.log(error);
+    }
+ }
+
+const getAllUsers = async (req, res) => { 
+    try {
+        const users = await User.find({}).select('username _id')
+        res.status(200).send(users)
+    } catch (error) {
+        console.log(error);
+        res.send('Something went wrong')
+    }
+ }
+
+module.exports = {
+    createUser,
+    getAllUsers
+}
